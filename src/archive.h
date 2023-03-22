5b92ccc7f3d403daed4636bf5098dd994d8c9934
YaPish <BaQuT@yandex.ru>
2023-03-22 10:44:16 +0300

    Update archive struct

--- src/archive.h
+++ src/archive.h
@@ -2,7 +2,21 @@
 #define _ARCHIVE_H_
 
 class Archive {
+private:
+    struct GENERAL {
+        uint8_t length;
+        uint8_t minVersion;
 
+        uint8_t compressionWordMethodId;
+        Bitset  wordSize;
+    } m_generalInformation;
+
+    struct CATALOGS {
+        
+    };
+
+public:
+    void
 };
 
 #endif
